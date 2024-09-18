3a89e959c04480d085808dd4a1d9fb0ff39f89f3
Supriyars <108995675+Supriyars@users.noreply.github.com>
2024-09-18 13:43:43 +0000

    modified

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,10 @@ const accountId = 14453;
 let accountEmail = "hitesh@gmail.com";
 var accountPassword = "12345";
 accountCity = "jaipur";
-
+/*
+prefer not to use var 
+bacause of block scope  and functional scope issue
+*/
 accountEmail="Srs@gmail.com";
 accountPassword = "21234";
 accountCity = "Banglore";
--- 01_basics/script.js
+++ 01_basics/script.js
@@ -1,10 +1,2 @@
 console.log("Supriya");
-const accountId = 14453;
-let accountEmail = "hitesh@gmail.com";
-var accountPassword = "12345";
-accountCity = "jaipur";
-
-accountEmail="Srs@gmail.com";
-accountPassword = "21234";
-accountCity = "Banglore";
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+ 
