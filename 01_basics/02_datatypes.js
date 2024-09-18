1dc0725ad0c2dcf0153f37dc0d9c61d71adb4a55
Supriyars <108995675+Supriyars@users.noreply.github.com>
2024-09-18 14:02:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS code as newer version
+//alert("Hello")// we are using node js ,not browser so avoid to write alert like this.
+ let name = "Armaan"
+ let age = 25;
+ let isStudying = true;
+ //number => 2 to the power 53
+ //bigint
+ //String => " "
+ //boolean => true/false
+ // null => standalone value but it is one of the datatype 
+ // undefined => 
+    //symbol => unique
+ console.log(name);
+ console.log(typeof(name));
+ console.log(typeof(null));
+ console.log(typeof(undefined));
+ console.log(typeof(isStudying));
