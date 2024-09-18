9797db8a4bf484cfcd1901a85d569ba71f9857e3
Supriyars <108995675+Supriyars@users.noreply.github.com>
2024-09-18 11:17:08 +0000

    config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 14453;
+let accountEmail = "hitesh@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+
+accountEmail="Srs@gmail.com";
+accountPassword = "21234";
+accountCity = "Banglore";
+console.table([accountId,accountEmail,accountPassword,accountCity]);
