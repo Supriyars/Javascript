79b4492984421d1284cc644ba600629968c89a49
Supriyars <108995675+Supriyars@users.noreply.github.com>
2024-09-19 10:33:41 +0000

    converstions

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,17 @@
+let score = "33abc";
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+let value = 33;
+let someNumber = String(value);
+console.log(typeof someNumber);
+console.log(someNumber)
+let num =1 ;
+let value1 = Boolean(num);
+console.log(value1);
+console.log(typeof value1);
+
+
+
+
