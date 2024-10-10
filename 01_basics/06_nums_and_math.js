2267722025fa5c830f58d5a51836b6cc8e967965
Supriyars <108995675+Supriyars@users.noreply.github.com>
2024-10-10 14:17:39 +0000

    commit

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,16 @@
+const score = 400;
+const num = new Number(1123.4);
+// console.log(num);
+// console.log(num.toString().length);//to find the length of number
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(4));
+const hundred= 10000000;
+// console.log(hundred.toLocaleString('en-IN'));
+//**************Maths**********************//
+console.log(Math);
+console.log(Math.abs(-5));///abs is used only to change -ve to +ve no;
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.0));
+// console.log(Math.floor(4.3));
+console.log(Math.random());
+console.log(Math.random()*10+1);
