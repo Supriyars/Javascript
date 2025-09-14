6dc0b90d453c520899f1c2d2c1a6dcc6de1231c2
Supriya Sangolli <108995675+Supriyars@users.noreply.github.com>
2025-09-14 18:27:29 +0000

    commit

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,21 @@
+//const tinderUser = new Object()
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "Supriya"
+tinderUser.isLoggedIn = false;
+//console.log(tinderUser);
+const regularUser = {
+  email: "some@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "supriya",
+      lastname: "sangolli" // Move this inside userfullname
+    }
+  }
+};
+//console.log(regularUser.fullname);
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+//const obj3 = {obj1, obj2}
+const obj3 = Object.assign({}, obj1,obj2)
+console.log(obj3)
