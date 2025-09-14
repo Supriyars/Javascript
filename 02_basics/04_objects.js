2f525864da48bd67532b7e2af03dd1ce679642d6
Supriya Sangolli <108995675+Supriyars@users.noreply.github.com>
2025-09-14 18:54:54 +0000

    commit

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -16,6 +16,12 @@ const regularUser = {
 //console.log(regularUser.fullname);
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
+const obj4 = {5:"a", 6:"b"}
 //const obj3 = {obj1, obj2}
-const obj3 = Object.assign({}, obj1,obj2)
-console.log(obj3)
+//const obj3 = Object.assign({}, obj1,obj2,obj4)
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3)
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
